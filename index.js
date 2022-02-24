731df4f286512a041cea4b2371907259fc55ef95
Alexia Velador <alexiavel01@gmail.com>
2022-02-24 11:19:24 -0700

    commit tree c4aae6246340d9d164d0004f09f442651ad04250

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+     if (pickUp < 42){
+        return 42 - pickUp;
+     }
+    return pickUp - 42;
+}
+
+function distanceFromHqInFeet(inFeet){
+    return distanceFromHqInBlocks(inFeet)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        return (destination - start)*264;
+    }else{
+        return (start - destination)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let disTravel = distanceTravelledInFeet(start, destination);
+    if (disTravel < 400) {
+        return 0;
+    } else if (disTravel > 400 && disTravel < 2000) {
+        return (disTravel - 400) * 0.02;
+    } else if (disTravel > 2000 && disTravel < 2500){
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
+
+//code from Thea to compare to mine:
+// function calculatesFarePrice(block1, block2) {
+//     let price = distanceTravelledInFeet(block1, block2);
+//     if (price < 400) {
+//       return 0;
+//     } else if (price > 400 && price < 2000) {
+//       return (price - 400) * 0.02;
+//     } else if (price > 2000 && price < 2500) {
+//       return 25;
+//     } else {
+//       return "cannot travel that far";
+//     }
+// }
